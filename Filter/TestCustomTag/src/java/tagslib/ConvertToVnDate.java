93c6f4d43aef1b8d557fb8f51f5c6998c1172b9f
namhm08 <161960081+namhm08@users.noreply.github.com>
2024-03-18 19:05:23 +0700

    complete ex1 custom tag

---
+++ Filter/TestCustomTag/src/java/tagslib/ConvertToVnDate.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/TagHandler.java to edit this template
+ */
+
+package tagslib;
+
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.tagext.JspFragment;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
+
+/**
+ *
+ * @author hoang
+ */
+public class ConvertToVnDate extends SimpleTagSupport {
+
+    private String value;
+
+    @Override
+    public void doTag() throws JspException {
+        JspWriter out = getJspContext().getOut();
+
+        try {
+            String[] s = value.split("-");
+            out.print(String.format("Ngay %s, thang %s, nam %s", s[0], s[1], s[2]));
+            
+            JspFragment f = getJspBody();
+            if (f != null) f.invoke(out);
+            
+
+        } catch (java.io.IOException ex) {
+            throw new JspException("Error in ConvertToVnDate tag", ex);
+        }
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
---
+++ Filter/TestCustomTag/web/ConvertToVnDate.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : ConvertToVnDate
+    Created on : Mar 18, 2024, 6:52:46 PM
+    Author     : hoang
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="mytag" uri="/WEB-INF/tlds/mycustomtagslib"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <mytag:ConvertToVnDate value="05-15-2024"/>
+    </body>
+</html>
